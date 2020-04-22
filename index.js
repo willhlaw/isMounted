2ab56e2a2d00a9865b9867281f0075040034e79e
willhlaw <will.lawrence@gmail.com>
2020-04-22 14:23:28 -0400

    Move ref isMounted fields to a current object

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const useIsMounted = () => {
     isMounted.current = true;
     isMounted.isLongDelay = () => false;
     return () => {
-      isMounted.current = false;
-      isMounted.unMountedTimestamp = Date.now();
-      isMounted.isLongDelay = (delayInMs = 3000) => isMounted.unMountedTimestamp - Date.now() > delayInMs;
-    }
+      isMounted.current = {
+        unMountedTimestamp: Date.now(),
+        isLongDelay: (delayInMs = 3000) => isMounted.unMountedTimestamp - Date.now() > delayInMs
+      };
   }, []);
   return isMounted;
 };
