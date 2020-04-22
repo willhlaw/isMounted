c45b20c9a13580bc08f708b692ed3511ec6380c6
willhlaw <will.lawrence@gmail.com>
2020-04-22 14:36:51 -0400

    Fix brackets

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const useIsMounted = () => {
   useEffect(() => {
     isMounted.current = true;
     isMounted.isLongDelay = () => false;
-    return () => {
+    return () =>
       isMounted.current = {
         unMountedTimestamp: Date.now(),
         isLongDelay: (delayInMs = 3000) => isMounted.unMountedTimestamp - Date.now() > delayInMs
-      };
+      }
   }, []);
   return isMounted;
 };
